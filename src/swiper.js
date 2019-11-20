71b75649b794c32ee0e814e87b34f8cd7d928ca6
MIBO <MIBO@SNPC0X7D4B>
2019-11-20 12:46:44 +0100

    Showroom start

---
+++ src/swiper.js
@@ -0,0 +1,22 @@
+fetch("../content/test.json")
+  .then(data => data.json())
+  .then(data => {
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+      let element = `<div class="swiper-slide"><img src="${data[i].img}"/></div>`;
+      swiper.appendSlide(element);
+    }
+  })
+  .catch(err => {
+    console.error(err);
+  });
+let swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  loop: true,
+  centeredSlides: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
