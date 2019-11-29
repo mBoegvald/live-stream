c00228c7eb892b7614d4dcb490eeed9f0c0bb333
David Fraenkel <davi1360@stud.kea.dk>
2019-11-29 13:11:32 +0100

    Overlay på slide og footer begyndelse

--- src/weather.js
+++ src/weather.js
@@ -1,43 +1,43 @@
-// function weather() {
-//     var location = document.getElementById("location");
-//     var apiKey = "ce4206500db0e67c73e3e4d2112e2687";
-//     var url = "https://api.forecast.io/forecast/";
+function weather() {
+  var location = document.getElementById("location");
+  var apiKey = "ce4206500db0e67c73e3e4d2112e2687";
+  var url = "https://api.forecast.io/forecast/";
 
-//     navigator.geolocation.getCurrentPosition(success, error);
+  navigator.geolocation.getCurrentPosition(success, error);
 
-//     function success(position) {
-//       latitude = position.coords.latitude;
-//       longitude = position.coords.longitude;
+  function success(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
 
-//       $.getJSON(
-//         url +
-//           apiKey +
-//           "/" +
-//           latitude +
-//           "," +
-//           longitude +
-//           "?lang=da&units=auto&callback=?",
-//         function(data) {
-//           console.log(
-//             url +
-//               apiKey +
-//               "/" +
-//               latitude +
-//               "," +
-//               longitude +
-//               "?lang=da&units=auto&?callback=?"
-//           );
-//           let temp = Math.round(data.currently.temperature);
-//           let summary = data.currently.summary;
-//           $("#temp").html(`${temp}`);
-//           $("#minutely").html(`${summary}`);
-//         }
-//       );
-//     }
+    $.getJSON(
+      url +
+        apiKey +
+        "/" +
+        latitude +
+        "," +
+        longitude +
+        "?lang=da&units=auto&callback=?",
+      function(data) {
+        console.log(
+          url +
+            apiKey +
+            "/" +
+            latitude +
+            "," +
+            longitude +
+            "?lang=da&units=auto&?callback=?"
+        );
+        let temp = Math.round(data.currently.temperature);
+        let summary = data.currently.summary;
+        $("#temp").html(`${temp}°`);
+        $("#minutely").html(`${summary}`);
+      }
+    );
+  }
 
-//     function error() {
-//       location.innerHTML = "Unable to retrieve your location";
-//     }
-//   }
+  function error() {
+    location.innerHTML = "Unable to retrieve your location";
+  }
+}
 
-//   weather();
+weather();
