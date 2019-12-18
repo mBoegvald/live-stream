c6a751b24a965365ec0361a9c7b34a858581dd24
miklasboegvald <mb@testaviva.dk>
2019-12-18 12:42:47 +0100

    David klarede det

--- src/scripts/SendMail.js
+++ src/scripts/SendMail.js
@@ -4,6 +4,11 @@ export default class SendMail {
       $("#myForm").submit(function(e) {
         e.preventDefault();
         let form_data = $(this).serialize();
+        let name = document.querySelector("input[name=name]").value;
+        let email = document.querySelector("input[name=email]").value;
+        if (!name || !email) {
+          return alert("Navn og Email er påkrævet");
+        }
         $.ajax({
           type: "POST",
           url: "form-to-email.php",
@@ -11,7 +16,7 @@ export default class SendMail {
           data: form_data
         })
           .done(function(data) {
-            alert("Din mail er blevet sendt!");
+            alert("Din email er sendt!");
           })
           .fail(function(data) {
             alert("Noget gik galt, prøv igen.");
